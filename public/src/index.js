08b959612905343ebd5fb469585e4a020aa4d973
Luke <l.avveduto00gmail.com>
2025-02-13 17:40:18 +0100

    modifiche form parziali

--- public/src/index.js
+++ public/src/index.js
@@ -1,11 +1,16 @@
 import { generateNavbar } from "./scripts/navbarComponent.js";
 import { generateNavigator } from "./scripts/navigatorComponent.js";
+import { generateForm } from "./scripts/formComponent.js";
 
 const pages = document.querySelector("#pages");
 const navbarContainer = document.querySelector("#nav-container");
+const formContainer = document.getElementById("formContainer")
 
+const modalForm= generateForm(modalForm,null)
 const navigator = generateNavigator(pages);
 const navComponent = generateNavbar(navbarContainer);
 
 navComponent.build(["home","admin"]);
 navComponent.render();
+
+modalForm.render();
--- public/src/scripts/formComponent.js
+++ public/src/scripts/formComponent.js
@@ -1,53 +1,34 @@
-export const generateForm = (parentElement, pubsub) => {
+export const generateForm = (parentElement, pubsub) => {    
     let onEdit;
     const formObject = {
         render : function() {
             /*
-            !-SOSTITUIRE L'HTML, INSERIRCI INSERIMENTO IMMAGINI
+            !-SOSTITUIRE L'HTML, INSERIRCI INSERIMENTO IMMAGINI, CORREGGERE CONTENUTO HTML VIENE SOSTITUITO DA SOLO TESTO ADD IMAGE, FARE RIFERIMENTO A GIT HUB
             */
             let html = 
-            `<form class="container-fluid">
+            `
+            <form class="container-fluid">
                 <div class="row md-row">
-                    <div>
-                    <button type="button" class="btn btn-danger clearForm" data-bs-dismiss="modal"><i class="fa-solid fa-ban"></i> Cancel</button>
-                    <button type="button" class="btn btn-dark clearForm" data-bs-dismiss="modal" id="submitButton"><i class="fa-solid fa-floppy-disk"></i> Save</button>
+                    <div class="col">
+                        <input type="text" class="form-control" placeholder="image" id="imageInput">
                     </div>
+                </div>
+                <div class="row md-row">
                     <div id="resultLabel" class="form-text text-danger text-center"></div>
                 </div>
             </form>
                             ` ;
             parentElement.innerHTML = html ;
-            document.getElementById("FormTitle").innerText = "Add image";
+            document.getElementById("formTitle").innerText = "Add image";
+            const inputFile = document.querySelector('#file');
 
             document.querySelectorAll(".clearForm").forEach(b => {
                 b.onclick = () => {
                     if (b.id === "submitButton") {
                         if (true) { //aggiungi condizione per quando è presente immagine 
-                            document.getElementById("adminFormTitle").innerText = "Add article";
+                            document.getElementById("fomeTitle").innerText = "Add image";
 
-                            if (playMainLink.value) newImages.push(playMainLink.value)
-                            if (playSecondLink.value) newImages.push(playSecondLink.value)
-                            if (playThirdLink.value) newImages.push(playThirdLink.value)
-    
-                            let article = {} ;
-                            let title = workTitleInput.value ;
-                            article.place = {
-                                "name": playLocation.value,
-                                "coords": []
-                            } ;
-                            article.yearofpub = playPubblicationYear.value ;
-                            article.era = playEra.value ;
-                            article.resume = textInput.value ;
-                            article.images = newImages ;
-                            article.characters = playCharacters.value ;
-                            newImages = [];
-
-                            let fullArticle = {
-                                "title": title,
-                                "article": article
-                            }
-                            
-                            pubsub.publish("form-submit", fullArticle);
+                            pubsub.publish("form-submit");
                         }
                         else {
                             resultLabel.innerText = "Not all forms compiled";
@@ -55,46 +36,22 @@ export const generateForm = (parentElement, pubsub) => {
                     }
                     else {
                         onEdit = false;
-                        document.getElementById("adminFormTitle").innerText = "Add article";
-                        workTitleInput.value = "" ;
-                        textInput.value = "" ;
-                        playMainLink.value = "" ;
-                        playSecondLink.value = "" ;
-                        playThirdLink.value = "" ;
-                        playLocation.value = "" ;
-                        playCharacters.value = "" ;
-                        playPubblicationYear.value = "" ;
-                        playEra.value = "" ;
+                        document.getElementById("formTitle").innerText = "Add image";
+                        //reset immagine
                         resultLabel.innerText = "";
                     }
                 };
             })
         },
-        setInputsValue : (title, articleDictionary) => {
+        setInputsValue : (image) => {
             onEdit = true;
-            document.getElementById("adminFormTitle").innerText = "Edit article (" + title + ")";
-            if (title) document.getElementById("workTitleInput").value = title ;
-            if (articleDictionary.resume) document.getElementById("textInput").value = articleDictionary.resume ;
-            if (articleDictionary.images[0]) document.getElementById("playMainLink").value = articleDictionary.images[0] ;
-            if (articleDictionary.images[1]) document.getElementById("playSecondLink").value =  articleDictionary.images[1] ;
-            if (articleDictionary.images[2]) document.getElementById("playThirdLink").value =  articleDictionary.images[2] ;
-            if (articleDictionary.place.name) document.getElementById("playLocation").value =  articleDictionary.place.name ;
-            if (articleDictionary.characters) document.getElementById("playCharacters").value = articleDictionary.characters ;
-            if (articleDictionary.yearofpub) document.getElementById("playPubblicationYear").value = articleDictionary.yearofpub ;
-            if (articleDictionary.era) document.getElementById("playEra").value = articleDictionary.era ;
+            document.getElementById("formTitle").innerText = "Change image";
+            if (image) document.getElementById("workTitleInput").value = title ; //correggere per fare riferimento a valore inserimento immagine
         },
         clear: () => {
             onEdit = false;
-            document.getElementById("adminFormTitle").innerText = "Add article";
-            document.getElementById("workTitleInput").value = "" ;
-            document.getElementById("textInput").value = "" ;
-            document.getElementById("playMainLink").value = "" ;
-            document.getElementById("playSecondLink").value = "" ;
-            document.getElementById("playThirdLink").value = "" ;
-            document.getElementById("playLocation").value = "" ;
-            document.getElementById("playCharacters").value = "" ;
-            document.getElementById("playPubblicationYear").value = "" ;
-            document.getElementById("playEra").value = "" ;
+            document.getElementById("formTitle").innerText = "Add Image";
+            //aggiungere svuota campo immagine
             document.getElementById("resultLabel").innerText = "" ;
         },
         setError: (error) => {
@@ -127,7 +84,7 @@ const renderList = async() => {
 (async () => {
     const inputFile = document.querySelector('#file');
     const button = document.querySelector("#button");
-    const errorDiv = document.querySelector("#error");
+    const errorDiv = document.querySelector("#error");x
   
     handleSubmit = async (event) => {
       const formData = new FormData();
