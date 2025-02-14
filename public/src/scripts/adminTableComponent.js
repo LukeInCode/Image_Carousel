5c9bbf3c91dd1cb459f096dd247821bb08644ab0
Luke <l.avveduto00gmail.com>
2025-02-14 18:15:46 +0100

    modifica admin table

--- public/src/scripts/adminTableComponent.js
+++ public/src/scripts/adminTableComponent.js
@@ -1,58 +1,44 @@
 export const generateAdminTable = (parentElement,pubsub) => {
-    let header = [];
-    let data = [];
-
+    let images = []
+    let callback;
     const tableObject = {
-        build: function(inputHeader, inputData) {
-            header = inputHeader;
-            data = inputData;
-            pubsub.subscribe("get-remote-data",(remoteData)=>{
-                this.setData(remoteData);
-                this.render();
-            })
+        build: function(imagesInput,callbackInput) {
+            images=imagesInput;
+            callback=callbackInput
         },
         render: function() {
-            let html = '<table class="table table-focus table-striped"><thead class="sticky-on-top"><tr>';
+            let html = '<table class="table table-focus table-striped"><thead class="sticky-on-top">';
             
-            header.forEach(e => {
-                html += '<th class="table-secondary">' + e + "</th>";
-            });
-            html += "</tr></thead><tbody>";
+            html += images.map((image) => 
+                `<tr><td><img src="${image.url}" class="d-block" alt="${image.url}"></td>`+
+                '<td><a href="#article-' + image + '"id="' + image + '" class="imageLink">' + e + ' <i class="fa-solid fa-arrow-up-right-from-square"></i></a></td><td> <button type="button" id="delete-' + e + '" class="btn btn-danger deleteButton"><i class="fa-solid fa-trash"></i> Delete</button></td></tr>'
+            ).join("");
 
-            let dataKeys = Object.keys(data);
 
-            /*
-            !-SOSTITUIRE ARTICOLO CON IMMAGINE-!
-            */
-            dataKeys.forEach(e => {
-                html += '<tr><td><a href="#article-' + e.replaceAll(" ", "-") + '"id="' + e + '" class="articleLink">' + e + ' <i class="fa-solid fa-arrow-up-right-from-square"></i></a></td><td><button type="button" id="edit-' + e + '" class="btn btn-warning editButton" data-bs-toggle="modal" data-bs-target="#modalForm"><i class="fa-solid fa-pen-to-square"></i> Edit</button> <button type="button" id="delete-' + e + '" class="btn btn-danger deleteButton"><i class="fa-solid fa-trash"></i> Delete</button></td></tr>';
-            });
-
-            html += "</tbody></table>";
+            html += "</tbody>";
+            html += '<tfoot><tr><td><button type="button" class="btn btn-info addButton"><i class="fa-solid fa-trash"></i> add Image</button></td></tr></tfoot></table>'
             parentElement.innerHTML = html;
 
-            document.querySelectorAll(".editButton").forEach(b => {
-                b.onclick = () => {
-                    const playTitle = b.id.replace("edit-", "");
-                    pubsub.publish("el-edited", playTitle);
-                };
-            });
 
             document.querySelectorAll(".deleteButton").forEach(b => {
                 b.onclick = () => {
                     const playTitle = b.id.replace("delete-", "");
                     
-                    delete data[playTitle];
+                    delete images[playTitle];
                     
-                    pubsub.publish("el-deleted", data);
+                    //pubsub.publish("el-deleted", data);
                 };
             });
+
+            document.querySelectorAll(".addButton").forEach(b => {
+                b.onclick = callback();
+            });
         },
-        setData: function(inputData) {
-            data = inputData;
+        setImages: function(inputImages) {
+            images = inputImages;
         },
-        getData: function() {
-            return data;
+        getImages: function() {
+            return images;
         }
     };
     return tableObject;
